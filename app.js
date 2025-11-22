9b56cf304b437d9e9bd1df469b483dc9553d9e4b
ishaq-khan123 <muhammadishaqk401@gmail.com>
2025-11-22 19:07:50 +0500

    Q one added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("hello world!");
+
+
+let speed = 45;
+
+if (speed <=30 ){
+    console.log("Too slow");
+    
+} else if (speed >= 30 && speed <=60){
+    console.log("Safe speed");
+    
+} else if (speed >= 61 && speed <= 100){
+    console.log("Speeding");
+    
+} else {
+    console.log("Dangerously fast");
+    
+}
