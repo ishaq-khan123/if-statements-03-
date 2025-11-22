62d9052f12cb3270d3f85c6d40d94e30b968737e
ishaq-khan123 <muhammadishaqk401@gmail.com>
2025-11-22 19:21:11 +0500

    Q  two added

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 console.log("hello world!");
 
 
-let speed = 45;
+// let speed = 45;
 
-if (speed <=30 ){
-    console.log("Too slow");
+// if (speed <=30 ){
+//     console.log("Too slow");
     
-} else if (speed >= 30 && speed <=60){
-    console.log("Safe speed");
+// } else if (speed >= 30 && speed <=60){
+//     console.log("Safe speed");
     
-} else if (speed >= 61 && speed <= 100){
-    console.log("Speeding");
+// } else if (speed >= 61 && speed <= 100){
+//     console.log("Speeding");
     
-} else {
-    console.log("Dangerously fast");
+// } else {
+//     console.log("Dangerously fast");
     
-}
+// }
+
+
+
+
+// let latedays = 0;
+
+// if (latedays === 0 , latedays * 0){
+//     console.log("$0 dollor, no due date");
+    
+// } else if (latedays >= 1 && latedays <=5 , latedays * 1){
+//     console.log("$1 dollor per day");
+    
+// } else if (latedays >=6 && latedays <=10 , latedays * 2){
+//     console.log("$5 dollor per day");
+    
+// } else {
+//     console.log("$5 doller per day");
+    
+// }
+
+
+
+
+
+
+
+
